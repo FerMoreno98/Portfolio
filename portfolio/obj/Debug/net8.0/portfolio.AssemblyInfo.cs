725bff3063e982b6f64d102fa481c5fad479ce67
Fer <fmorenomarzal@gmail.com>
2025-01-03 08:55:50 +0100

    imagenes,css,animaciones

--- portfolio/obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ portfolio/obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71913ab23d3deebfe353473066b3fecb3239b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ portfolio/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿
+document.addEventListener('DOMContentLoaded', () => {
+
+    asignarListenerAlScroll();
+    asignarListenerModalEmail()
+})
+function asignarListenerAlScroll() {
+    document.addEventListener("scroll", function () {
+        const navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {  
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+
+}
+
+function asignarListenerModalEmail() {
+    const modalemail = document.getElementById('contactme');
+    modalemail.addEventListener('click', () => {
+
+        const modal = new bootstrap.Modal(document.getElementById('ModalEmail'));
+        modal.show();
+    })
+}
